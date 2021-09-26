b50f6462e817713646ea5d9e2bee38f875925ddf
trucanhtran <anhttt18411@st.uel.edu.vn>
2021-09-26 12:05:12 +0700

    generate new plugin polls

--- plugins/polls/init.rb
+++ plugins/polls/init.rb
@@ -5,6 +5,9 @@ Redmine::Plugin.register :polls do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  require_dependency 'polls_hook_listener'
+
+  settings default: {'empty' => true}, partial: 'settings/poll_settings'
 
   menu :project_menu, :polls, { controller: 'polls', action: 'index' }, caption: 'Polls', after: :activity, param: :project_id
 
